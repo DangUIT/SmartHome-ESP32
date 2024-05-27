3a86ae4194223391f8ab87776df4f061d862e18a
Le Huu Dat <146201680+datlh9103@users.noreply.github.com>
2024-05-27 22:29:55 +0700

    Update app_main.c

--- main/app_main.c
+++ main/app_main.c
@@ -24,6 +24,24 @@
 #include "dht11.h"
 #include "SSD1306.h"
 
+#define _I2C_NUMBER(num) I2C_NUM_##num
+#define I2C_NUMBER(num) _I2C_NUMBER(num)
+
+#define DATA_LENGTH 512                  /*!< Data buffer length of test buffer */
+#define RW_TEST_LENGTH 128               /*!< Data length for r/w test, [0,DATA_LENGTH] */
+#define DELAY_TIME_BETWEEN_ITEMS_MS 1000 /*!< delay time between different test items */
+
+#define I2C_MASTER_SCL_IO 22               /*!< gpio number for I2C master clock */
+#define I2C_MASTER_SDA_IO 21               /*!< gpio number for I2C master data  */
+#define I2C_MASTER_NUM I2C_NUMBER(0) /*!< I2C port number for master dev */
+#define I2C_MASTER_FREQ_HZ 100000        /*!< I2C master clock frequency */
+#define I2C_MASTER_TX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+#define I2C_MASTER_RX_BUF_DISABLE 0                           /*!< I2C master doesn't need buffer */
+
+#define SSD1306_ADDR OLED_I2C_ADDRESS   /*!< slave address for BH1750 sensor */
+#define WRITE_BIT I2C_MASTER_WRITE              /*!< I2C master write */
+#define READ_BIT I2C_MASTER_READ                /*!< I2C master read */
+
 static const char *TAG = "MQTT_EXAMPLE";
 
 float humidity,temperature;
@@ -105,6 +123,25 @@ static void mqtt_app_start(void)
     esp_mqtt_client_start(client);
 }
 
+static esp_err_t i2c_master_init(void)
+{
+    int i2c_master_port = I2C_MASTER_NUM;
+    i2c_config_t conf = {
+        .mode = I2C_MODE_MASTER,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .sda_pullup_en = GPIO_PULLUP_ENABLE,
+        .scl_io_num = I2C_MASTER_SCL_IO,
+        .scl_pullup_en = GPIO_PULLUP_ENABLE,
+        .master.clk_speed = I2C_MASTER_FREQ_HZ,
+        // .clk_flags = 0,          /*!< Optional, you can use I2C_SCLK_SRC_FLAG_* flags to choose i2c source clock here. */
+    };
+    esp_err_t err = i2c_param_config(i2c_master_port, &conf);
+    if (err != ESP_OK) {
+        return err;
+    }
+    return i2c_driver_install(i2c_master_port, conf.mode, I2C_MASTER_RX_BUF_DISABLE, I2C_MASTER_TX_BUF_DISABLE, 0);
+}
+
 void app_main(void)
 {
     ESP_LOGI(TAG, "[APP] Startup..");
@@ -130,17 +167,16 @@ void app_main(void)
     ESP_ERROR_CHECK(example_connect());
 
     mqtt_app_start();
-
+    ESP_ERROR_CHECK(i2c_master_init());                         
+    ssd1306_init(); 
     while(1){
+    task_ssd1306_display_clear();    
     dht_read_float_data(DHT_TYPE_DHT11, 17, &humidity, &temperature);
     // printf("Sensor data: humidity=%.1f, temp=%d\n", humidity, temperature);
     // printf("Humidity: %.1f%%\nTemp: %.1fC\n", humidity, temperature);
     sprintf(data,"Humidity: %.1f%%\nTemp: %.1fC\n", humidity, temperature);
+    task_ssd1306_display_text(data);
     esp_mqtt_client_publish(client, "dht", data, strlen(data), 0, 0);
     vTaskDelay(10000 / portTICK_PERIOD_MS);
     }
 }
-
-
-
-
